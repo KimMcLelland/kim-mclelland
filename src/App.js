63937e29bac5a3dda3c6e26b0844dec40cb59486
Kim <faeriekim3@gmail.com>
2021-07-15 11:44:12 +0100

    beginning display

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Kim McLelland</h1>
+      <div className="intro">
+        <p>This is a promotional web app, showcasing my skills</p>
+        <p>with JS React and also providing information about me.</p>
+        <p>What do you want to know about me?</p>
+      </div>
+      <div className="start_page_buttons">
+        <button>Education</button>
+        <button>Creativity</button>
+        <button>Spirituality</button>
+        <button>Gender and Sexuality</button>
+      </div>
+      <p className="footer">Bakcground image by <a href="https://unsplash.com/@weirick?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jake Weirick</a> on <a href="https://unsplash.com/s/photos/stars?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  </p>
     </div>
   );
 }
