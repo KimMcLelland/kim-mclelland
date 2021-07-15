947c7c1e9fa3b01bf86e6cedf7eafe559bb9374d
Kim <faeriekim3@gmail.com>
2021-07-15 14:42:05 +0100

    different pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [page, setPage] = useState(0);
+
+  const pageDisplay = () => {
+    if (page === 0) {
+      return(
+        <div className="start_page_buttons">
+          <button onClick={() => setPage(page+1)}>Education</button>
+          <button onClick={() => setPage(page+2)}>Creativity</button>
+          <button onClick={() => setPage(page+3)}>Spirituality</button>
+          <button onClick={() => setPage(page+4)}>Gender and Sexuality</button>
+        </div>
+        )
+    }
+    if (page === 1) {
+      return (
+        <div className = "education_page">
+        <p>This is my education</p>
+        <button onClick={() => setPage(page-1)}>back to main page</button>
+        </div>
+      )
+    }
+    if (page === 2) {
+      return(
+        <div className = "creativity_page">
+          <p>These are my creative achievements</p>
+          <button onClick={() => setPage(page-2)}>back to main page</button>
+          </div>
+      )
+    }
+    if (page === 3) {
+      return(
+        <div className = "spirituality_page">
+          <p>A history of my spiritual beliefs</p>
+          <button onClick={() => setPage(page-3)}>back to main page</button>
+        </div>
+      )
+    }
+    if (page === 4) {
+      return (
+        <div className = "lgbt_page">
+          <p>I am trans, submissive, polyamorous and bisexual. So there!</p>
+          <button onClick={() => setPage(page-4)}>back to main page</button>
+        </div>
+      )
+    }
+  }
+
   return (
     <div className="App">
       <h1>Kim McLelland</h1>
@@ -10,11 +57,8 @@ function App() {
         <p>with JS React and also providing information about me.</p>
         <p>What do you want to know about me?</p>
       </div>
-      <div className="start_page_buttons">
-        <button>Education</button>
-        <button>Creativity</button>
-        <button>Spirituality</button>
-        <button>Gender and Sexuality</button>
+      <div>
+        {pageDisplay()}
       </div>
       <p className="footer">Bakcground image by <a href="https://unsplash.com/@weirick?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jake Weirick</a> on <a href="https://unsplash.com/s/photos/stars?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   </p>
